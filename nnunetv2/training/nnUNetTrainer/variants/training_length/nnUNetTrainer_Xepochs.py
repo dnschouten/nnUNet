1ca8849bd0bc972d08930b01ecca758694736395
Daan Schouten <69320678+dnschouten@users.noreply.github.com>
2025-05-21 10:22:10 -0700

    added 224 patch trainer

--- nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py
+++ nnunetv2/training/nnUNetTrainer/variants/training_length/nnUNetTrainer_Xepochs.py
@@ -118,6 +118,13 @@ class nnUNetTrainer_300epochs_192patch(nnUNetTrainer):
         self.num_epochs = 300
         self.save_every = 10
 
+class nnUNetTrainer_300epochs_224patch(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
+        self.num_epochs = 300
+        self.save_every = 10
+
 class nnUNetTrainer_350epochs(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
                  device: torch.device = torch.device('cuda')):
